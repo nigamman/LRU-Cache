f27d57677e4d3df7dcbcc7c23a30788415c953bc
Shivansh Nigam <shivanshnigam07@gmail.com>
2026-01-30 21:07:43 +0530

    project running as expected

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
+using namespace std;
 #include "LRUCache.h"
+#include <windows.h>
+#define sleep(x) Sleep(1000 * x)
 
 int main() {
     LRUCache cache(2);
@@ -7,17 +10,17 @@ int main() {
     cache.put(1, 100);
     cache.put(2, 200);
 
-    std::cout << cache.get(1) << std::endl; // 100
+    cout << cache.get(1) << endl; // 100
 
     cache.put(3, 300); // evicts key 2
 
-    std::cout << cache.get(2) << std::endl; // -1
-    std::cout << cache.get(3) << std::endl; // 300
+    cout << cache.get(2) << endl; // -1
+    cout << cache.get(3) << endl; // 300
 
     cache.put(4, 400, 2); // TTL = 2 seconds
     sleep(3);
 
-    std::cout << cache.get(4) << std::endl; // -1 (expired)
+    cout << cache.get(4) << endl; // -1 (expired)
 
     return 0;
 }
